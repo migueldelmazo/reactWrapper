73271c0f6e98bdd20d3556fac596fc350df7ae3b
Miguel del Mazo <migueldelmazo@gmail.com>
2015-08-14 12:43:08 +0200

    Create: StoreClass

---
+++ app/lib/store.js
@@ -0,0 +1,25 @@
+'use strict';
+
+import _ from 'lodash';
+import Atom from './atom';
+
+var StoreClass = function (options) {
+    _.extend(this, options);
+    Atom.on(this);
+  };
+
+StoreClass.prototype.getAtom = function (attr) {
+  return Atom.get(attr);
+};
+
+StoreClass.prototype.setAtom = function (attr, value) {
+  return Atom.set(attr, value);
+};
+
+module.exports = {
+
+  create: function (options) {
+    return new StoreClass(options);
+  }
+
+};
--- app/stores/Addresses.js
+++ app/stores/Addresses.js
@@ -1,29 +1,44 @@
 'use strict';
 
-import Atom from '../lib/atom';
+import StoreClass from '../lib/store';
 
-var store = {
-
-  atom: {
+var atomAttr = {
     address: 'address',
     payment: 'payment',
     price: 'foo.price',
     user: 'user'
+  };
+
+export default StoreClass.create({
+
+  atomAttr: atomAttr,
+
+  atomListener: [
+    {
+      atom: atomAttr.address,
+      actions: [
+        {
+          action: 'onAddressChange'
+        }
+      ]
+    }
+  ],
+
+  onAddressChange: function () {
+    console.log(this.getAtom(atomAttr.address));
   },
 
   receiveAddresses () {
     setTimeout(function () {
-      Atom.set(store.atom.address, 'My street');
-      Atom.set(store.atom.payment, 'Visa');
-      Atom.set(store.atom.price, 'Price');
-      Atom.set(store.atom.user, 'Miguel');
-    }, 1000);
+      this.setAtom(this.atomAttr.address, 'My street');
+      this.setAtom(this.atomAttr.payment, 'Visa');
+      this.setAtom(this.atomAttr.price, 'Price');
+      this.setAtom(this.atomAttr.user, 'Miguel');
+    }.bind(this), 1000);
   },
 
   foo () {
     return 1234;
   }
 
-};
-
-export default store
+});
