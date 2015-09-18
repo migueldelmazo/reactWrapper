9e4505c440ba48cca5e5e43d630346b7d02f4bc1
Miguel del Mazo <migueldelmazo@gmail.com>
2015-09-18 17:00:36 +0200

    Wrap routes store with store class

--- app/stores/router.js
+++ app/stores/router.js
@@ -1,66 +1,70 @@
 'use strict';
 
 import _ from 'lodash';
-import Atom from '../lib/atom';
 import KeyRouter from 'KeyRouter';
+import StoreClass from '../lib/store';
 
 var atomAttr = {
     routes: 'router.routes',
     currentName: 'router.currentName',
     currentValues: 'router.currentValues'
-  };
+  },
 
-// init KeyRouter listeners
+  updateAtom = function (routes) {
+    var currentRouter = _.last(routes);
+    this.setAtom(atomAttr.routes, routes);
+    this.setAtom(atomAttr.currentName, currentRouter.name);
+    this.setAtom(atomAttr.currentValues, currentRouter.values);
+  },
 
-KeyRouter.onChangeHash(function (routes) {
-  var currentRouter = _.last(routes);
-  Atom.set(atomAttr.routes, routes);
-  Atom.set(atomAttr.currentName, currentRouter.name);
-  Atom.set(atomAttr.currentValues, currentRouter.values);
-});
+  appRoutes = [
+    {
+      name: 'index',
+      path: '',
+      subRoute: [
+        {
+          name: 'addresses',
+          path: 'addresses',
+          subRoute: [
+            {
+              name: 'addressAdd',
+              path: 'add',
+              subRoute: [
+                {
+                  name: 'addressAddAll',
+                  path: 'addAll'
+                }
+              ]
+            },
+            {
+              name: 'addressUser',
+              path: ':user',
+              subRoute: [
+                {
+                  name: 'addressId',
+                  path: ':id'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          name: 'payments',
+          path: 'payments'
+        }
+      ]
+    }
+  ];
 
-// set app routes
+export default StoreClass.create({
 
-KeyRouter.init([
-  {
-    name: 'index',
-    path: '',
-    subRoute: [
-      {
-        name: 'addresses',
-        path: 'addresses',
-        subRoute: [
-          {
-            name: 'addressAdd',
-            path: 'add',
-            subRoute: [
-              {
-                name: 'addressAddAll',
-                path: 'addAll'
-              }
-            ]
-          },
-          {
-            name: 'addressUser',
-            path: ':user',
-            subRoute: [
-              {
-                name: 'addressId',
-                path: ':id'
-              }
-            ]
-          }
-        ]
-      },
-      {
-        name: 'payments',
-        path: 'payments'
-      }
-    ]
-  }
-]);
+  init () {
+    KeyRouter.onChangeHash(updateAtom.bind(this));
+    KeyRouter.init(appRoutes);
+  },
 
-export default {
-  atomAttr: atomAttr,
+  atomAttr,
+
   notFound: 'notFound'
-};
+
+});
